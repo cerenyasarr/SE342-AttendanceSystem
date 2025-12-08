ae588dfcd027e1f3b5d0543186128604a31e8f54
Mehmet Senadli <mehmetsenadli44@gmail.com>
2025-12-08 16:09:59 +0300

    change local db with production db

--- backend/config.py
+++ backend/config.py
@@ -11,8 +11,9 @@ class Config:
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
-    
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DB_URL = os.getenv('DATABASE_URL', default=None)
+    print("Database URL:", DB_URL)
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" if DB_URL is None else DB_URL
     
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload size
